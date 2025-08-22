156fcfa786555917034f9072366ce65f1463826b
Jude Zalme <jz0008@morningside.edu>
2025-08-22 14:16:44 -0500

    commit

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # csci310-test-repo.
-This is jude zalme's test repository for CS310./n
-does this make new line?
+This is jude zalme's test repository for CS310.    
---
+++ src/hello.py
@@ -0,0 +1,12 @@
+hello.py
+====================================
+This is a quick sample script.
+| Jude Zalme
+| 2025 August 20
+"""
+print("hello world")
+def something():
+"""
+This function does nothing.
+"""
+pass
